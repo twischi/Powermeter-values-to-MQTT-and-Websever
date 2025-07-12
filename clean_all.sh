2250a75466a3f656e252fa560246d9b5b7c0ca88
twischi <32617619+twischi@users.noreply.github.com>
2025-07-12 14:31:28 +0200

    Version 1.6.6: Updated serval files

--- clean_all.sh
+++ clean_all.sh
@@ -15,3 +15,10 @@ echo "> Done 'idf.py fullclean'"
 echo 
 echo "Cleaned All to virgin state."
 echo 
+echo 
+echo "If you are using VSCode ESP-IDF extension:"
+echo "   (1) QUIT VSCode and RESTART it"
+echo "   (2) Choose a Project Configuration from status bar"
+echo "   (3) Build the project again"
+echo "   (4) Set the Serial-port before flashing"
+echo
